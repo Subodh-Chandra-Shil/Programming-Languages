b451d348d3a3171193c714af6b16d7a412d46176
Subodh Chandra Shil <golang602@gmail.com>
2024-07-02 22:39:18 +0600

    Python assignment added

---
+++ Python/Python Data Structures/dictionary.py
@@ -0,0 +1,16 @@
+demoDict = {
+    'name': 'Subodh',
+    'age': 24,
+    'email': 'subodh@gmail.com',
+    'email': 'subodh.shil@gmail.com',
+}
+
+print(demoDict)
+print(demoDict['name'])
+print(len(demoDict))
+
+
+
+
+
+
